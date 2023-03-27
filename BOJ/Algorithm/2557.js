6d10c24c91d07a66c2a43d71ebe28155c3c3e0df
JaeHyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2023-03-27 16:58:56 +0900

    BOJ_Hello World

--- BOJ/Algorithm/2557.js
+++ BOJ/Algorithm/2557.js
@@ -1,10 +1,10 @@
 // 백준 입력 받을 때
 // const fs = require("fs");
 // const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-// const input = fs.readFileSync(filePath).toString().split("\n");
+// let input = fs.readFileSync(filePath).toString().split("\n");
 // 2줄로 줄이면
 // const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-// const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+// let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
 
 // 입력으로 문자열을 받을 때
 // const fs = require("fs");
