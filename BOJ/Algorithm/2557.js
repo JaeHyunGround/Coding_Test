c668c73cf422907dae929bfe984fb1b0acb1597c
JaeHyunGround <gothddlek@naver.com>
2023-03-27 16:57:26 +0900

    BOJ_Hello World

--- BOJ/Algorithm/2557.js
+++ BOJ/Algorithm/2557.js
@@ -1,4 +1,14 @@
+// 백준 입력 받을 때
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// let input = fs.readFileSync(filePath).toString().split("\n");
+// 2줄로 줄이면
 // const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 // const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+// 입력으로 문자열을 받을 때
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// let input = fs.readFileSync(filePath).toString().trim();
 
 console.log("Hello world!");
