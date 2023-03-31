287d41d2a0335c2c9cfeb28fdabf3e9e9f0ea809
JaeHyunGround <gothddlek@naver.com>
2023-04-01 01:16:46 +0900

    BOJ_엄청난 부자2

--- BOJ/B5/1271.js
+++ BOJ/B5/1271.js
@@ -6,6 +6,7 @@ let money = BigInt(input[0]); // 가진 돈
 let member = BigInt(input[1]); // 돈 받으러 온 생명체
 // 엄~~~~~~청난 부자이기 때문에 BigInt를 사용해야 한다.
 // 숫자 제한이 10^1000 이하 까지 이므로 큰 수다.
+// 엄청 큰 숫자로 연산을 해야할 때 BigInt() => toString() 루트로 가자
 
 let Nmoney = (money / member).toString(); // 생명체 하나에게 돌아가는 돈의 양
 let left_money = (money % member).toString(); // 분배할 수 없는 남는 돈
