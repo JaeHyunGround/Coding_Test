ce5a70787d7aa58e129a8d21638dacea327b97b6
JaeHyunGround <gothddlek@naver.com>
2023-03-31 14:53:05 +0900

    BOJ_A-B

--- BOJ/Algorithm/Jr/1000.js
+++ BOJ/Algorithm/Jr/1000.js
@@ -2,5 +2,6 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/Algorithm/input.txt";
 let input = fs.readFileSync(filePath).toString().split(" ");
 
+
 var answer = Number(input[0]) + Number(input[1]);
 console.log(answer);
