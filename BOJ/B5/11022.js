a97978f0ff339c6d47655ed917e90ba16e4fb9a8
JaeHyunGround <gothddlek@naver.com>
2023-04-03 17:44:18 +0900

    BOJ_A+B - 8

--- BOJ/B5/11022.js
+++ BOJ/B5/11022.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(filePath).toString().split("\n");
 
 const case_num = Number(input[0]);
 
-for(var i = 0; i < case_num; i++) {
-    let data = input[i+1].toString().split(" ");
-    console.log("Case #" + (i+1) + ": " + Number(data[0]) + " + " + Number(data[1]) + " = " + (Number(data[0]) + Number(data[1])));
+for(var i = 1; i <= case_num; i++) {
+    let data = input[i].toString().split(" ");
+    console.log("Case #" + (i) + ": " + Number(data[0]) + " + " + Number(data[1]) + " = " + (Number(data[0]) + Number(data[1])));
 }
