14eb21f63d189090045af65e20f6a5bda0578611
JaeHyunGround <gothddlek@naver.com>
2023-04-03 16:29:36 +0900

    BOJ_놀람

--- BOJ/B5/10926.js
+++ BOJ/B5/10926.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
 let input = fs.readFileSync(filePath).toString().trim(" ");
 // 문자열을 입력 받을 땐 .trim() 으로 받자 !
-// trim() : 문자열 양 끝의 공백을 제거한 문자열을 반환해줌.
+// trim() : 문자열 양 끝의 공백을 제거한 문자열을 반환해줌. 
 
 console.log(input + "??!");
