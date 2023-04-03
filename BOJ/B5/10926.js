9fbe736a6e41ab4fef2b4ed3ed41e738d3781729
JaeHyunGround <gothddlek@naver.com>
2023-04-03 16:29:04 +0900

    BOJ_10926

--- BOJ/B5/10926.js
+++ BOJ/B5/10926.js
@@ -4,4 +4,4 @@ let input = fs.readFileSync(filePath).toString().trim(" ");
 // 문자열을 입력 받을 땐 .trim() 으로 받자 !
 // trim() : 문자열 양 끝의 공백을 제거한 문자열을 반환해줌.
 
-console.log(input + "??!")
+console.log(input + "??!");
