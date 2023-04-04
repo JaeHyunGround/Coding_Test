90f6fc2979afb6ddd9e204cfeeccacab974e9eff
JaeHyunGround <gothddlek@naver.com>
2023-04-04 10:46:01 +0900

    BOJ_단어의 개수

--- BOJ/B2/1152.js
+++ BOJ/B2/1152.js
@@ -5,5 +5,5 @@ let input = fs.readFileSync(filePath).toString().trim();
 let words = input.toString().split(" ")
 
 console.log(words[0] === "" ? 0: words.length);
-// 빈 값에 들어오는 테스트 케이스를 생각 해야한다.
+// 빈 값에 들어오는 테스트 케이스를 생각 해야한다. (공백 1개로만 이루어진 문자열)
 // 따라서 조건문으로 빈 값이 들어올 떄를 걸러줘야 한다.
