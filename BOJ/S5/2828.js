92dadd0465921f0ae54fabb9a659549eb12d6eca
JaeHyunGround <gothddlek@naver.com>
2023-04-14 14:24:13 +0900

    BOJ_사과 담기 게임

--- BOJ/S5/2828.js
+++ BOJ/S5/2828.js
@@ -1,32 +1,26 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
+// .trim()을 안해줘서 틀렸다......
 
-const [N,M] = input[0].split(' ').map(a=>Number(a)) // N : 스크린, M : 바구니 길이
-const apple_num = Number(input[1]) // 사과의 갯수
-const apples = input.slice(2,input.length).map(a=> Number(a)) 
-// input의 인덱스 2번부터 input 길이 미만의 인덱스 번호까지 배열로 뽑아낸다.
- 
-let answer = 0
-let start = 1 // 바구니의 첫 부분
-let end = M // 바구니의 마지막 부분
+let basket = Number(input[0].toString().split(" ")[1]);
+let move = Number(0); // 움직인 횟수
 
-// for..of
-// 반복가능한 객체에 대해서 반복하는 루프 생성
-// 즉 apples의 길이 만큼 for문을 돌고 apples를 돌 때 해당 요소는 apple
-// map과 유사한 매커니즘. 결과값은 아예 다름
-for(let apple of apples){
-    if (end < apple){
-        answer += apple - end
-        end = apple
-        start = apple - (M - 1)
-    }
+let start = Number(1);
+let end = basket;
 
-    else if (start > apple){
-        answer += start - apple
-        start = apple
-        end = apple + (M - 1)
+for (var i = 2; i < input.length; i++) {
+    if (Number(input[i]) < start || Number(input[i]) > end) {
+        if(Number(input[i]) < start) {
+            move += (start - Number(input[i]));
+            end -= (start - Number(input[i]))
+            start = Number(input[i]);
+        } else if (Number(input[i]) > end) {
+            move += (Number(input[i]) - end);
+            start += (Number(input[i]) - end);
+            end = Number(input[i]);
+        }
     }
 }
 
-console.log(answer)
+console.log(move);
