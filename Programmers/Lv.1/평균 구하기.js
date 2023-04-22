e1a6d39a71801012544e235869790d32cf75f740
JaeHyunGround <gothddlek@naver.com>
2023-04-22 17:57:34 +0900

    PG_평균 구하기

--- Programmers/Lv.1/평균 구하기.js
+++ Programmers/Lv.1/평균 구하기.js
@@ -10,3 +10,14 @@ function solution(arr) {
     
     return answer;
 }
+
+
+// 버전 2 : reduce 사용
+function solution(arr) {
+    var answer = 0;
+    let sum = arr.reduce((a, b) => a + b);
+    
+    answer = sum / arr.length;
+    
+    return answer;
+}
