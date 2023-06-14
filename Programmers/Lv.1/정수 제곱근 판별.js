f45ac499d008989a56640b6b069dd966e5bf3081
JaeHyunGround <gothddlek@naver.com>
2023-06-14 11:04:40 +0900

    PG_정수 제곱근 판별

--- Programmers/Lv.1/정수 제곱근 판별.js
+++ Programmers/Lv.1/정수 제곱근 판별.js
@@ -2,7 +2,7 @@ function solution(n) {
     var answer = 0;
     
     let sqrt = Math.sqrt(n); // 제곱근을 구해줌. Math.sqrt(4) = 2
-    let isInt = parseInt(sqrt) === sqrt;
+    let isInt = parseInt(sqrt) === sqrt; // 변수엔 식이 들어갈 수 있다 !!!!!
     
     if (isInt) {
         answer = Math.pow((sqrt + 1), 2); // Math.pow(2, 2) = 2^2 = 4
