16cc1d95a5e31165092cd61086b3accb70a292dc
JaeHyunGround <gothddlek@naver.com>
2023-07-26 12:12:26 +0900

    BOJ & FC_나이순 정렬

--- JS_coding_test/Part 1/정렬 문제 풀이/Q4.js
+++ JS_coding_test/Part 1/정렬 문제 풀이/Q4.js
@@ -1,7 +1,8 @@
 // 백준 11004
 
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "JS_coding_test/input.txt";
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : "JS_coding_test/input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 let [n, k] = input[0].toString().trim().split(" ").map(Number); // n : 숫자 갯수, k : 찾아야 할 위치
---
+++ JS_coding_test/Part 1/정렬 문제 풀이/Q9.js
@@ -0,0 +1,34 @@
+// 백준 10814
+
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : "JS_coding_test/input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+input.shift(); // case 갯수 제거
+let arr = []; // case들을 담을 배열
+
+// 데이터가 나이, 이름 2가지 이므로 나이순 정렬을 위해 객체화 시켜준다.
+for (let i = 0; i < input.length; i++) {
+  let one_case = input[i].split(" ");
+  let cases = {};
+
+  cases.age = one_case[0];
+  cases.name = one_case[1];
+
+  arr.push(cases);
+}
+
+// 나이 순 정렬 but 나이가 같다면 가입한 순서대로
+// 나이가 같지 않은 때만 고려하여 정렬해준다.
+// why ? 나이가 같을 땐 이미 가입한 순서대로 정렬이 되어있기 때문 !!
+arr.sort(function (a, b) {
+  if (a.age !== b.age) return a.age - b.age;
+});
+
+let answer = []; // 정답을 담을 배열
+for (let j = 0; j < arr.length; j++) {
+  answer.push(`${arr[j].age} ${arr[j].name}`);
+}
+
+console.log(answer.join("\n"));
