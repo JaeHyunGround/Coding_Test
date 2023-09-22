d4427edff91af0e9348770d678786fb63d23305a
JaeHyunGround <gothddlek@naver.com>
2023-09-22 15:18:46 +0900

    PG_올바른 괄호 풀이 추가

--- Programmers/Lv.2/올바른 괄호.js
+++ Programmers/Lv.2/올바른 괄호.js
@@ -1,22 +1,40 @@
-function solution(s){
-    var answer = true;
-    let input = s.split("");
-    let count = 0;
-    
-    if(input[0] == ')' || input[input.length - 1] == '(') {
-        answer = false;
+function solution(s) {
+  // 풀이 1
+  // var answer = true;
+  // let input = s.split("");
+  // let count = 0;
+
+  // if(input[0] == ')' || input[input.length - 1] == '(') {
+  //     answer = false;
+  // } else {
+  //     for(var i = 0; i < input.length; i++) {
+  //         input[i] == '(' ? count++ : count--;
+  //         if(count == -1) {
+  //             answer = false;
+  //             break;
+  //         }
+  //     }
+  //     answer = count !== 0 ? false : true
+  // }
+
+  // return answer;
+
+  // 풀이 2
+  let count = 0;
+
+  // 배열이나 문자열 돌 떈 for - of
+  for (const c of s) {
+    if (c === "(") {
+      count++;
     } else {
-        for(var i = 0; i < input.length; i++) {
-            input[i] == '(' ? count++ : count--;
-            if(count == -1) {
-                answer = false;
-                break;
-            }
-        }
-        answer = count !== 0 ? false : true
+      if (count === 0) {
+        return false;
+      }
+      count--;
     }
-    
-    return answer;
+  }
+
+  return count === 0;
 }
 
-// 예외적인 상황을 앞으로 뺴두기 
+// 예외적인 상황을 앞으로 뺴두기
