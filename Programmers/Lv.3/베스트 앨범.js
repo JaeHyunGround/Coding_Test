1daf9745e28e4aacc949465637cae25abf99c603
JaeHyunGround <gothddlek@naver.com>
2023-09-25 16:23:14 +0900

    PG_베스트 앨범

---
+++ Programmers/Lv.3/베스트 앨범.js
@@ -0,0 +1,22 @@
+// '묶는다' 라는 개념이 필요하다면 해시 테이블 (Map 객체)을 이용하여 푸는 것이 대부분이다.
+
+function solution(genres, plays) {
+  const genreMap = new Map();
+
+  genres
+    .map((genre, index) => [genre, plays[index]])
+    .forEach(([genre, play], index) => {
+      const data = genreMap.get(genre) || { total: 0, songs: [] };
+      genreMap.set(genre, {
+        total: data.total + play,
+        songs: [...data.songs, { play, index }]
+          .sort((a, b) => b.play - a.play)
+          .slice(0, 2),
+      });
+    });
+
+  return [...genreMap.entries()]
+    .sort((a, b) => b[1].total - a[1].total)
+    .flatMap((item) => item[1].songs)
+    .map((item) => item.index);
+}
