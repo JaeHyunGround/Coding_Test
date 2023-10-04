fd9c610f79154469bde7d1dd5155f278a297961a
JaeHyunGround <gothddlek@naver.com>
2023-10-04 15:05:41 +0900

    BOJ_이제는 더 이상 물러날 곳이 없다_상수_ 피드백 반영

--- BOJ/B2/2908.js
+++ BOJ/B2/2908.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split(" ");
 
+// 풀이 1
 // let num1 = input[0].toString().split("").reverse();
 // let num2 = input[1].toString().split("").reverse();
 
@@ -23,8 +24,17 @@ let input = fs.readFileSync(filePath).toString().trim().split(" ");
 //     }
 // }
 
-let num1 = input[0].toString().split("").reverse().join("");
-let num2 = input[1].toString().split("").reverse().join("");
+// 풀이 2
+// let num1 = input[0].toString().split("").reverse().join("");
+// let num2 = input[1].toString().split("").reverse().join("");
 
-const answer = [num1, num2].sort(); // 오름차순 정렬
-console.log(answer[1]); // 큰 값은 인덱스 1번에 존재하니 answer[1] 출력
+// const answer = [num1, num2].sort((a, b) => a - b); // 오름차순 정렬
+// console.log(answer[1]); // 큰 값은 인덱스 1번에 존재하니 answer[1] 출력
+
+// 풀이 3
+// map 함수로 중복되는 코드를 줄인다 !! - 스터디 개선점
+// 기존 2개의 풀이느 input이 2개 이상이면 쓸 수가 없는 코드였다.
+let answer = input
+  .map((num) => num.toString().split("").reverse().join(""))
+  .sort((a, b) => a - b);
+console.log(answer[answer.length - 1]);
