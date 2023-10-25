af882ecaec7f0800687f9ec120d47a2bc40a1d51
JaeHyunGround <gothddlek@naver.com>
2023-10-25 15:08:24 +0900

    BOJ_이제는 더 이상 물러날 곳이 없다_괄호의 값

--- BOJ/G5/2504.js
+++ BOJ/G5/2504.js
@@ -1,31 +1,80 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("");
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let [strings] = input;
+
+function solution(strings) {
+  const string = strings.split("");
+  const stack = [];
+
+  if (!checkPossiable(string)) {
+    return 0;
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    let top = stack[stack.length - 1];
+    const cur = string[i];
+
+    if (cur === "(" || cur === "[") {
+      stack.push(cur);
+    } else if (cur === ")" || cur === "]") {
+      const reverse = cur === ")" ? "(" : "[";
+      const point = reverse === "(" ? 2 : 3;
+
+      if (top === reverse) {
+        stack.pop();
+        stack.push(point);
+      } else {
+        let temp = 0;
+        while (1) {
+          const pop = stack.pop();
+          if (typeof pop === "number") {
+            temp += pop;
+          } else if (pop === reverse) {
+            stack.push(temp * point);
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  return stack.reduce((acc, cur) => acc + cur);
+}
 
 // 올바른 괄호열인지 체크
-function checkPossiable(string_array) {
+function checkPossiable(string) {
   const stack = [];
   let temp = "";
 
-  string_array.forEach((item) => {
-    if (item === "(") stack.push("(");
-    else if (item === "[") stack.push("[");
-    else if (item === ")") {
+  // 중간에 멈추는 등 기대하는 결과를 얻기 위해서는 그냥 for문을 사용하자.
+  // string.forEach((item) => {
+  //   if (item === "(") stack.push("(");
+  //   else if (item === "[") stack.push("[");
+  //   else if (item === ")") {
+  //     temp = stack.pop();
+  //     if (temp !== "(") return false;
+  //   } else if (item === "]") {
+  //     temp = stack.pop();
+  //     if (temp !== "[") return false;
+  //   }
+  // });
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "(") stack.push("(");
+    else if (string[i] === "[") stack.push("[");
+    else if (string[i] === ")") {
       temp = stack.pop();
       if (temp !== "(") return false;
-    } else if (item === "]") {
+    } else if (string[i] === "]") {
       temp = stack.pop();
       if (temp !== "[") return false;
     }
-  });
-
-  return stack.length === 0;
-}
-
-function solution(string_array) {
-  if (!checkPossiable(string_array)) {
-    return 0;
   }
+
+  return stack.length ? false : true;
 }
 
-solution(input);
+const answer = solution(strings);
+console.log(answer);
--- JS_coding_test/Part 1/그리디 알고리즘/Q7.js
+++
@@ -1,6 +0,0 @@
-// 백준 1946
-
-const fs = require("fs");
-const filePath =
-  process.platform === "linux" ? "/dev/stdin" : "JS_coding_test/input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
