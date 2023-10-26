8fa5a515aebbddd86452839097d0763419f79a11
JaeHyunGround <gothddlek@naver.com>
2023-10-27 00:32:43 +0900

    BOJ_2164 카드2

---
+++ BOJ/S4/2164.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(value) {
+    this.queue[this.rear++] = value;
+  }
+
+  dequeue() {
+    const value = this.queue[this.front];
+    delete this.queue[this.front];
+    this.front++;
+    return value;
+  }
+
+  peek() {
+    return this.queue[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+}
+
+const queue = new Queue();
+
+for (let i = 1; i <= Number(input); i++) {
+  queue.enqueue(i);
+}
+
+while (queue.size() !== 1) {
+  queue.dequeue();
+  let temp = queue.dequeue();
+  queue.enqueue(temp);
+}
+
+console.log(queue.peek());
