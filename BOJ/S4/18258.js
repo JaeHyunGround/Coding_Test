20475e9317fceb0d668dfa528e07cacd2c9cacbb
JaeHyunGround <gothddlek@naver.com>
2023-10-27 00:19:53 +0900

    BOJ & FC_18258 큐 2

--- BOJ/S4/18258.js
+++ BOJ/S4/18258.js
@@ -2,67 +2,83 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-// // pop, shift 등 배열로 구현하면 시간초과 난다 !!!!!
+input.shift();
+class Queue {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
 
-// input.shift(); // 제일 첫 번째 요소인 명령의 수 제거
+  enqueue(value) {
+    this.queue[this.rear++] = value;
+  }
 
-// // 큐는 배열로 구현하면 좋진 않다. 하지만 이 문제에선 배열로 구현해본다.
-// // 큐는 연결 리스트로 구현하는 것이 좋음
+  dequeue() {
+    if (this.front === this.rear) {
+      return -1;
+    } else {
+      const value = this.queue[this.front];
+      delete this.queue[this.front];
+      this.front += 1;
+      return value;
+    }
+  }
 
-// let queue = [];
-// let answer = [];
+  empty() {
+    if (this.front === this.rear) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 
-// const push = (queue, num) => {
-//   queue.push(num);
-// };
+  size() {
+    return this.rear - this.front;
+  }
 
-// const pop = (queue) => {
-//   let temp = queue.shift();
-//   if (temp) answer.push(temp);
-//   else answer.push(-1);
-// };
+  isFront() {
+    if (this.front !== this.rear) {
+      return this.queue[this.front];
+    } else {
+      return -1;
+    }
+  }
 
-// const size = (queue) => {
-//   answer.push(queue.length);
-// };
+  isBack() {
+    if (this.front !== this.rear) {
+      return this.queue[this.queue.length - 1];
+    } else {
+      return -1;
+    }
+  }
+}
 
-// const empty = (queue) => {
-//   if (queue.length === 0) answer.push(1);
-//   else answer.push(0);
-// };
+const queue = new Queue();
+let answer = [];
 
-// const front = (queue) => {
-//   if (queue[0]) answer.push(queue[0]);
-//   else answer.push(-1);
-// };
+input.forEach((input) => {
+  let cases = input.split(" ");
+  switch (cases[0]) {
+    case "push":
+      queue.enqueue(Number(cases[1]));
+      break;
+    case "pop":
+      answer.push(queue.dequeue());
+      break;
+    case "size":
+      answer.push(queue.size());
+      break;
+    case "empty":
+      answer.push(queue.empty());
+      break;
+    case "front":
+      answer.push(queue.isFront());
+      break;
+    case "back":
+      answer.push(queue.isBack());
+      break;
+  }
+});
 
-// const back = (queue) => {
-//   if (queue[queue.length - 1]) answer.push(queue[queue.length - 1]);
-//   else answer.push(-1);
-// };
-
-// input.forEach((input) => {
-//   let cases = input.split(" ");
-//   switch (cases[0]) {
-//     case "push":
-//       push(queue, cases[1]);
-//       break;
-//     case "pop":
-//       pop(queue);
-//       break;
-//     case "size":
-//       size(queue);
-//       break;
-//     case "empty":
-//       empty(queue);
-//       break;
-//     case "front":
-//       front(queue);
-//       break;
-//     case "back":
-//       back(queue);
-//       break;
-//   }
-// });
-
-// console.log(answer.join("\n"));
+console.log(answer.join("\n"));
