bd5e94407cc9ace05c0713b00e89c2684208aad8
JaeHyunGround <gothddlek@naver.com>
2023-10-27 00:20:26 +0900

    BOJ_18258 큐 2

--- BOJ/S4/18258.js
+++ BOJ/S4/18258.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
-
 input.shift();
+
 class Queue {
   constructor() {
     this.queue = [];
