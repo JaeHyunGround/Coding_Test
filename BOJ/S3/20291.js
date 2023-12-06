061b6fe083d58ef9c2d9b0ab610763be74a464fe
JaeHyunGround <gothddlek@naver.com>
2023-12-06 10:10:26 +0900

    BOJ_20291 파일 정리

--- BOJ/S3/20291.js
+++ BOJ/S3/20291.js
@@ -1,5 +1,27 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "BOJ/input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
+function solution(input) {
+  const test_case = input.shift();
 
+  const myMap = new Map();
+  input.forEach((line) => {
+    const fileExtension = line.split(".")[1].trim();
+
+    if (!myMap.has(fileExtension)) {
+      myMap.set(fileExtension, 1);
+    } else {
+      myMap.set(fileExtension, myMap.get(fileExtension) + 1);
+    }
+  });
+
+  const answer = [...myMap.keys()]
+    .sort()
+    .map((item) => `${item} ${myMap.get(item)}`)
+    .join("\n");
+
+  return answer;
+}
+
+console.log(solution(input));
